326323d4c294f80b8fef44ee24a7dc6a9a4d8a59
Anush <anush.raghunathan25@gmail.com>
2021-01-07 17:13:09 +0530

    Variables

---
+++ Variables/src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+
+        int firstNumber = 5;
+        int secondNumber = 12;
+        int thirdNumber = 3;
+        int sumNumbers =  firstNumber + secondNumber +thirdNumber;
+        System.out.println(sumNumbers);
+    }
+}
